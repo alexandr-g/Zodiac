a637865b4fea7bf04c43f3355d8683c99066656e
Alexandr <alexgrischuk@gmail.com>
2015-07-03 19:14:14 +0300

    Add login view

---
+++ Login.js
@@ -0,0 +1,80 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+    Text,
+    View,
+    Image,
+    Component
+} = React;
+
+class Login extends Component {
+	constructor(props){
+        super(props);
+
+        this.state = {
+            showProgress: false
+        }
+    }
+
+    render() {
+    	return ( 
+    		<View style={styles.container}>
+    			<Text>Hello</Text>
+    		</View>
+    	)
+    }
+}
+
+var styles = React.StyleSheet.create({
+    container: {
+        backgroundColor: '#F5FCFF',
+        paddingTop: 40,
+        padding: 10,
+        alignItems: 'center',
+        flex: 1
+    },
+    logo: {
+        width: 66,
+        height:55
+    },
+    heading: {
+        fontSize: 30,
+        margin: 10,
+        marginBottom: 20
+    },
+    loginInput: {
+        height: 50,
+        marginTop: 10,
+        padding: 4,
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: '#48BBEC',
+        borderRadius: 0,
+        color: '#48BBEC'
+    },
+    button: {
+        height: 50,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        alignSelf: 'stretch',
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 24
+    },
+    loader: {
+        marginTop: 20
+    },
+    error: {
+        color: 'red',
+        paddingTop: 10
+    }
+});
+
+module.exports = Login;
---
+++ index.ios.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} = React;
+
+var Login = require('./Login');
+
+var Zodiac = React.createClass({
+  render: function() {
+    return (
+      <Login />
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('Zodiac', () => Zodiac);
