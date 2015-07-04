ff0cd9e5ec17c7df17bddc2797825e6fc92910fe
Alexandr <alexgrischuk@gmail.com>
2015-07-04 19:50:54 +0300

    Add login fields with background image

--- Login.js
+++ Login.js
@@ -2,11 +2,14 @@
 
 var React = require('react-native');
 
-var {
+let {
     Text,
     View,
+    StyleSheet,
     Image,
-    Component
+    Component,
+    TextInput,
+    TouchableHighlight
 } = React;
 
 class Login extends Component {
@@ -19,40 +22,83 @@ class Login extends Component {
     }
 
     render() {
-    	return ( 
-    		<View style={styles.container}>
-    			<Text>Hello</Text>
-    		</View>
+    	return (
+    		
+    		<View style={styles.header}>
+                <Image style={styles.backgroundImage} source={require('image!background') } > 
+                <Text style={styles.heading}> Zodiac - Your Horoscope - 2015 </Text>
+				</Image>
+				<TextInput
+	                onChangeText={(text)=> this.setState({username: text})}
+	                style={styles.loginInput}
+	                placeholder="Github username"></TextInput> 
+				<TextInput
+	                onChangeText={(text)=> this.setState({password: text})}
+	                style={styles.loginInput}
+	                placeholder="Github password" secureTextEntry="true"></TextInput>
+	            <TouchableHighlight
+	                // onPress={this.onLoginPressed.bind(this)}
+	                style={styles.button}>
+	                <Text style={styles.buttonText}>Sign in</Text>
+	            </TouchableHighlight>
+            </View>
     	)
     }
 }
 
-var styles = React.StyleSheet.create({
+let styles = StyleSheet.create({
     container: {
-        backgroundColor: '#F5FCFF',
-        paddingTop: 40,
-        padding: 10,
+        paddingTop: 150,
+        padding: 100,
         alignItems: 'center',
         flex: 1
+    },
+     header: {
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        flexDirection: 'column',
+        paddingTop: 25,
+    },
+    headerText: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: 'white'
     },
     logo: {
         width: 66,
         height:55
     },
+    backgroundImage: {
+        paddingTop: 60,
+        flex: 1,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: -100
+    },
+    bgImageWrapper: {
+        position: 'absolute',
+        top: 0, bottom: 0, left: 0, right: 0
+    },
     heading: {
-        fontSize: 30,
-        margin: 10,
-        marginBottom: 20
+        fontSize: 25.2,
+        color: '#F0FFFF',
+        backgroundColor: '#3F51B5'
     },
     loginInput: {
+        color: '#F0FFFF',
         height: 50,
         marginTop: 10,
         padding: 4,
         fontSize: 18,
         borderWidth: 1,
         borderColor: '#48BBEC',
-        borderRadius: 0,
-        color: '#48BBEC'
+        borderRadius: 10,
+        backgroundColor: '#3F51B5',
+        bottom: -350
     },
     button: {
         height: 50,
@@ -62,7 +108,8 @@ var styles = React.StyleSheet.create({
         marginTop: 10,
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 5
+        borderRadius: 10,
+        bottom: -350
     },
     buttonText: {
         color: '#fff',
